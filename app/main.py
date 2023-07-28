14a70ddf229f5c27d238598f0d2c8a66d243a54c
Khazar Hajiyev <khazarhajiyev@hotmail.com>
2023-07-28 17:20:46 +0400

    added redis

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from redis import Redis
+
+redisDB = Redis(host='redis-db', port=6379)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    redisDB.incr('count')
+    count = str(redisDB.get('count'), encoding='utf-8')
+    return f"<p>Hello, World! {count}</p>"
